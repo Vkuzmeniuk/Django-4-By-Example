20107ef6a4727c6d807f9e5272d1f6e22ed67383
Vkuzmeniuk <vladislav.kuzmeniuk@gmail.com>
2023-02-21 19:37:03 +0100

    day 2

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-# Create your models here.
+from django.urls import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -14,8 +19,13 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2,choices=Status.choices,default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishedManager()
     class Meta:
         ordering = ['-publish']
         indexes = [models.Index(fields=['-publish'])]
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.id])
+    

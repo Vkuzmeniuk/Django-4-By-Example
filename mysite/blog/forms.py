1235a84753aed4c310364e912376ae6da0c6f877
Vkuzmeniuk <vladislav.kuzmeniuk@gmail.com>
2023-03-07 01:20:11 +0100

    Day 7

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -12,5 +12,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
+
+class SearchForm(forms.Form):
+    query = forms.CharField()
     
     
